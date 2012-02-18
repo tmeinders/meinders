1a397af54473f9b38405ee886f3b5d4fd71b3e1d
unknown <tmeinders@IBM-D8481DFAE60.(none)>
2012-02-18 17:59:17 -0500

    referencing file

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile('./testhelloworld.html', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 3000;
