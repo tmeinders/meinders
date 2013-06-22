2cff546b789c7f51acccebe0b3991ff6827dd06c
Travis Meinders <tmeinders@gmail.com>
2013-06-22 03:56:20 -0400

    reorganizing for my new meinders.com site

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('./testhelloworld.html', function(error, content) {
+  fs.readFile('./index.html', function(error, content) {
         if (error) {
             response.writeHead(500);
             response.end();
